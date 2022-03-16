158d38d69521b68102924865b557ae5995b27620
Sam <spatterson380@gmail.com>
2022-03-16 16:41:35 +0000

    testing more hello world functionality

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +8,13 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/hello")
+async def hello():
+    return {"message": "Hello 👋"}
+
+@app.get("/hello/{name}")
+async def hello_name(name: str, extra: Optional[str] = None):
+    if extra is None:
+        return {"message": f"Hello {name} 👋"}
+    return {"message": f"Hello {name} 👋 {extra} 😎"}
