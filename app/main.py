b2731b959e649a5d766e37c21ba065dfdff958b9
Sam <spatterson380@gmail.com>
2022-03-17 10:31:44 +0000

    adding pytube test and logging

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
+import logging
+
 from typing import Optional
 
 from fastapi import FastAPI
 
-app = FastAPI()
+
+logger = logging.getLogger('app')
+
+app = FastAPI(debug=True)
 
 
 @app.get("/")
@@ -18,3 +23,18 @@ async def hello_name(name: str, extra: Optional[str] = None):
     if extra is None:
         return {"message": f"Hello {name} 👋"}
     return {"message": f"Hello {name} 👋 {extra} 😎"}
+
+@app.get("/test")
+async def test():
+    from pytube import YouTube
+    yt = YouTube('http://youtube.com/watch?v=2lAe1cqCOXo')
+    streams = yt.streams
+    mp4_streams = streams.filter(only_video=True, file_extension='mp4')
+    for streams in mp4_streams:
+        logger.info(f"Video itag : {streams.itag} Resolution : {streams.resolution} VCodec : {streams.codecs[0]}")
+    video = mp4_streams.first()
+    fname = video.download('../downloads/', filename_prefix=f"[{yt.video_id}] ")
+    return {
+        "message": "Download finished",
+        "fname": fname
+        }
