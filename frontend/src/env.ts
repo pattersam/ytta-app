248e17ce692458e0fc88cecb640d4b31f90aed1b
Sam <spatterson380@gmail.com>
2022-03-22 16:29:42 +0000

    fixing reference to backend api
    it's only http for now (i.e. not http**s**)

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -3,7 +3,7 @@ const env = process.env.VUE_APP_ENV;
 let envApiUrl = '';
 
 if (env === 'production') {
-  envApiUrl = `https://${process.env.VUE_APP_DOMAIN_PROD}`;
+  envApiUrl = `http://${process.env.VUE_APP_DOMAIN_PROD}`;
 } else if (env === 'staging') {
   envApiUrl = `https://${process.env.VUE_APP_DOMAIN_STAG}`;
 } else {
