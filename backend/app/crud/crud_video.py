36dd2908830e24acd84761c56a5a24cb11ac9a8d
Sam <spatterson380@gmail.com>
2022-03-23 16:22:28 +0000

    hooking back up to pytube

--- backend/app/crud/crud_video.py
+++ backend/app/crud/crud_video.py
@@ -1,19 +1,39 @@
+import logging
+
 from typing import List
 
 from fastapi.encoders import jsonable_encoder
+from sqlalchemy import exc
 from sqlalchemy.orm import Session
+from pytube import YouTube
 
 from app.crud.base import CRUDBase
 from app.models.video import Video
-from app.schemas.video import VideoCreate, VideoUpdate
+from app.schemas.video import VideoBase, VideoCreate, VideoUpdate
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def create_new_video(url: str) -> VideoBase:
+    yt = YouTube(url)
+    # logger.info(f"Downloading: {url}")
+    # yt.streams.filter(only_video=True, file_extension='mp4').first().download('downloads/', filename_prefix=f"[{yt.video_id}] ")
+    return VideoBase(
+        title=yt.title,
+        description=yt.description,
+        url=url,
+        yt_id=yt.video_id,
+    )
 
 
 class CRUDVideo(CRUDBase[Video, VideoCreate, VideoUpdate]):
     def create_with_owner(
         self, db: Session, *, obj_in: VideoCreate, owner_id: int
     ) -> Video:
-        obj_in_data = jsonable_encoder(obj_in)
-        db_obj = self.model(**obj_in_data, owner_id=owner_id)
+        new_video_data = jsonable_encoder(create_new_video(obj_in.url))
+        db_obj = self.model(**new_video_data, owner_id=owner_id)
         db.add(db_obj)
         db.commit()
         db.refresh(db_obj)
