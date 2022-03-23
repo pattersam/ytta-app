90c827916dcda75318cffd84539010153e77ca11
Sam <spatterson380@gmail.com>
2022-03-23 16:23:06 +0000

    handle exceptions explicitly
    makes sure CORS is included so the make it through to the frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from starlette.middleware.cors import CORSMiddleware
+import sqlalchemy.exc
+import pytube.exceptions
 
 from app.api.api_v1.api import api_router
 from app.core.config import settings
 
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 app = FastAPI(
     title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
 )
+
+@app.exception_handler(sqlalchemy.exc.IntegrityError)
+async def integrety_error_exception_handler(request: Request, exc: sqlalchemy.exc.IntegrityError):
+    logger.error(exc)
+    return JSONResponse(
+        status_code=409,
+        content={"message": f"Database integrity error..."},
+    )
+
+@app.exception_handler(pytube.exceptions.RegexMatchError)
+async def pytube_regex_exception_handler(request: Request, exc: pytube.exceptions.RegexMatchError):
+    logger.error(exc)
+    return JSONResponse(
+        status_code=406,
+        content={"message": f"Unable to read URL..."},
+    )
 
 # Set all CORS enabled origins
 if settings.BACKEND_CORS_ORIGINS:
