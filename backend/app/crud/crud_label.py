2503e9195f0ac3ec9d30d299d5108ee7a6c1fc02
Sam <spatterson380@gmail.com>
2022-04-13 09:36:50 +0000

    create label occurances in video analysis function

--- backend/app/crud/crud_label.py
+++ backend/app/crud/crud_label.py
@@ -12,5 +12,11 @@ class CRUDLabel(CRUDBase[Label, LabelCreate, LabelUpdate]):
     def get_by_name(self, db: Session, *, name: str) -> Optional[Label]:
         return db.query(Label).filter(Label.name == name).first()
 
+    def get_or_create_by_name(self, db: Session, *, name: str) -> Optional[Label]:
+        label = self.get_by_name(db, name=name)
+        if label is None:
+            label = self.create(db, obj_in=LabelCreate(name=name))
+        return label
+
 
 label = CRUDLabel(Label)
